bdcba99632218b2fa4bba7c131d72ae7feaac46c
lxlfoo <lxlfoo@gmail.com>
2025-11-14 17:12:48 +0900

    feat: Create func

--- monty.py
+++ monty.py
@@ -1,1 +1,14 @@
-print('hello')
+import random
+
+def monty_hall_simulation(trials=100_000):
+    stay_wins = 0
+    switch_wins = 0
+
+# for문 (hhs)
+
+    return stay_wins, switch_wins
+
+if __name__ == "__main__":
+    stay, switch = monty_hall_simulation(100_000)
+    print(f"유지 성공 횟수: {stay}")
+    print(f"변경 성공 횟수: {switch}")
